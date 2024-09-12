62f529cb1ef1f10a814bdff27a3fa697359b7225
Nikola Gajic <nikolagajic191201@gmail.com>
2024-09-12 16:30:33 +0200

    Kreiranje modela, kontrolera i rute za kategoriju-Category

--- galleries/server/index.js
+++ galleries/server/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDB } from './database/connectDB.js';
 import authRoutes from './routes/auth.route.js';
+import categoryRoutes from './routes/category.route.js';
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/categories', categoryRoutes);
 
 app.listen(port, () => {
   connectDB();
